4593387bd73b971e4000b8231cc4a9a9f638dc6c
Avery P <89486736+Garymuo@users.noreply.github.com>
2022-01-18 19:21:39 -0500

    Add files via upload

---
+++ Week2/Celsius.py
@@ -0,0 +1,3 @@
+celsius = int(input("What is the temperature in celsius"))
+fahrenheit = (celsius * (9/5)) + 32
+print(fahrenheit)
---
+++ Week2/PayRate.py
@@ -0,0 +1,5 @@
+hours = int(input("How many hours did you work?"))
+rate = int(input("What rate were you working at?"))
+
+total = hours * rate
+print("You made", total, "dollars!")
