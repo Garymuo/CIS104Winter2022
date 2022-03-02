bda9ec8673be7ae9d5f4470f33795e706c9adf4a
Avery P <89486736+Garymuo@users.noreply.github.com>
2022-03-01 19:12:13 -0500

    Add files via upload

---
+++ Week7/7E1.py
@@ -0,0 +1,3 @@
+fhand = open(r'C:\Users\avery\Desktop\CIS104\mbox-short.txt')
+ope = fhand.read()
+print(ope.upper())
---
+++ Week7/7E3.py
@@ -0,0 +1,28 @@
+fname = input("Input File Address/File Name:")
+
+try:
+    fhand = open(fname)
+except:
+    if fname == "na na boo boo":
+        print("NA NA BOO BOO TO YOU - You have been punk'd!")
+        quit()
+    else :
+        print("File Cannot Be Opened, Try Again.")
+        quit()
+
+total = 0
+count = 0
+
+for line in fhand :
+    if not 'X-DSPAM-Confidence:' in line :
+        continue
+    position = line.find(':')
+    number = float(line[position+1 : ])
+    count = count + 1
+    total = total + number
+    print(count, line[19 : ])
+
+average = total/count
+print("Average Spam Confidence:", average)
+
+#X-DSPAM-Confidence
